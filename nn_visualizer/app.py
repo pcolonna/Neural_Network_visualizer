fffcc741be12c8511c98ace9c504569372f55221
Pierre Colonna D'Istria <pierre.col.di@gmail.com>
2021-03-07 22:38:56 +0100

    add a mini server

--- nn_visualizer/app.py
+++ nn_visualizer/app.py
@@ -6,7 +6,7 @@ st.title('Neural Network Visualizer')
 st.sidebar.title('Parameters')
 
 num_layers = st.sidebar.slider('Number of Layers', min_value=2, max_value=10, step=1)
-hidden_units_per_layers = st.sidebar.slider('Number of Layers', min_value=2, max_value=16, step=1)
+hidden_units_per_layers = st.sidebar.slider('Number of hidden units per layers', min_value=2, max_value=16, step=1)
 
 batch_size = st.sidebar.slider('Batch Size', min_value=16, max_value=256, step=16)
 
