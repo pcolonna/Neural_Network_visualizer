5e0d286b6a683e2daf84299d2492a3d93fc8a39d
Pierre Colonna D'Istria <pierre.col.di@gmail.com>
2021-03-07 22:03:21 +0100

    Starting on the interface

---
+++ nn_visualizer/app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import numpy as np 
+
+st.title('Neural Network Visualizer')
+
+st.sidebar.title('Parameters')
+
+num_layers = st.sidebar.slider('Number of Layers', min_value=2, max_value=10, step=1)
+hidden_units_per_layers = st.sidebar.slider('Number of Layers', min_value=2, max_value=16, step=1)
+
+batch_size = st.sidebar.slider('Batch Size', min_value=16, max_value=256, step=16)
+
+if st.sidebar.button('Train the new model'):
+    pass
+
+
+if st.sidebar.button('Get random predictions'):
+    pass
+
+st.sidebar.markdown('## Input Image')
+
+image_canvas = st.sidebar.image(np.zeros((28,28)), width=150)
